f6f55b3f6507d5dc06b9dd77005049ac1b69204d
Porawit Poboonma <ball6847@gmail.com>
2018-01-30 15:06:41 +0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,17 @@
 
 SEMAPHORE_URL=http://159.65.10.143:3000
 
+# -----------------------------------
+# update docker tag in semaphore
+
 read -r -d '' BODY <<- EOM
     {
       "id": 1,
-      "name":"deployment",
+      "name": "deployment",
       "project_id": 1,
       "password": null,
       "json": "{ \"docker_tag\": \"${DRONE_COMMIT_SHA}\"  }",
-      "removed":false
+      "removed": false
     }
 EOM
 
@@ -18,3 +21,18 @@ curl -X PUT --header 'Content-Type: application/json' \
   --header "Authorization: Bearer ${SEMAPHORE_TOKEN}" \
   -d "${BODY}" \
   ${SEMAPHORE_URL}/api/project/1/environment/1 -v
+
+# ---------------------------------------
+# enqueue tasks
+
+read -r -d '' BODY <<- EOM
+  {
+    "template_id": 1
+  }
+EOM
+
+curl -X POST --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  --header "Authorization: Bearer ${SEMAPHORE_TOKEN}" \
+  -d "${BODY}" \
+  ${SEMAPHORE_URL}/api/project/1/tasks -v
