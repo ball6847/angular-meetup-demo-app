cb79d57274e64d33f33494c3cc4b99a00fe00e08
Porawit Poboonma <ball6847@gmail.com>
2018-01-30 14:50:29 +0700

    reformat shell script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,19 @@
 
 SEMAPHORE_URL=http://159.65.10.143:3000
 
+read -r -d '' BODY <<- EOM
+    {
+      "id": 1,
+      "name":"deployment",
+      "project_id": 1,
+      "password": null,
+      "json": "{ \"docker_tag\": \"${DRONE_COMMIT_SHA}\"  }",
+      "removed":false
+    }
+EOM
+
 curl -X PUT --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
-  --header 'Authorization: Bearer ${SEMAPHORE_TOKEN}' \
-  -d '{ "docker_tag": "${DRONE_COMMIT_SHA}" }' \
+  --header "Authorization: Bearer ${SEMAPHORE_TOKEN}" \
+  -d "${BODY}" \
   ${SEMAPHORE_URL}/api/project/1/environment/1 -v
