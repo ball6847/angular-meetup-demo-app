1ad60f280e7ba6054194fcf49e30a0c84f866d0e
Porawit Poboonma <ball6847@gmail.com>
2018-01-30 14:41:11 +0700

    use put method

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 SEMAPHORE_URL=http://159.65.10.143:3000
 
-curl -X POST --header 'Content-Type: application/json' \
+curl -X PUT --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
   --header 'Authorization: Bearer ${SEMAPHORE_TOKEN}' \
   -d '{ "docker_tag": "${DRONE_COMMIT_SHA}" }' \
