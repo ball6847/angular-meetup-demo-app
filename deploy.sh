7e653c595ce874fcdcd7852be7910b3706569f31
Porawit Poboonma <ball6847@gmail.com>
2018-01-30 16:15:55 +0700

    backto token

--- deploy.sh
+++ deploy.sh
@@ -4,39 +4,7 @@
 # ENV: YOU HAVE TO INJECT THESE VARIABLES THROUGH DRONE SECRETS
 
 # SEMAPHORE_URL
-# SEMAPHORE_USERNAME
-# SEMAPHORE_PASSWORD
-
-echo ${SEMAPHORE_URL}
-echo ${SEMAPHORE_USERNAME}
-echo ${SEMAPHORE_PASSWORD}
-
-# -----------------------------------
-# login to get cookie
-
-read -r -d '' BODY <<- EOM
-  {
-    "auth": "${SEMAPHORE_USERNAME}",
-    "password": "${SEMAPHORE_PASSWORD}"
-  }
-EOM
-
-echo 'BODY='
-echo $BODY
-
-curl -v -b /tmp/semaphore-cookie -X POST \
-  --header 'Content-Type: application/json' \
-  --header 'Accept: application/json' \
-  -d "${BODY}" \
-  ${SEMAPHORE_URL}/api/auth/login 2>&1
-
-OUTPUT=`curl -vs -b /tmp/semaphore-cookie ${SEMAPHORE_URL}/api/user/tokens 2>&1`
-
-echo "OUTPUT=$OUTPUT"
-
-echo "$OUTPUT" | grep -Po "(?<=id\":\")[^\"]+(?=\")"
-
-echo "TOKEN=$TOKEN"
+# SEMAPHORE_TOKEN
 
 # -----------------------------------
 # update docker tag in semaphore
@@ -55,7 +23,7 @@ EOM
 curl -vs -X PUT \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
-  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Authorization: Bearer ${SEMAPHORE_TOKEN}" \
   -d "${BODY}" \
   ${SEMAPHORE_URL}/api/project/1/environment/1 2>&1
 
@@ -71,6 +39,6 @@ EOM
 curl -vs -X POST \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
-  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Authorization: Bearer ${SEMAPHORE_TOKEN}" \
   -d "${BODY}" \
   ${SEMAPHORE_URL}/api/project/1/tasks 2>&1
